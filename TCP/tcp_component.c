34b679ec5e129fe4b1c3cc70bab3b0328af3e466
mangpo <mangpo@eecs.berkeley.edu>
2016-11-08 23:22:03 -0800

    more code

--- TCP/tcp_component.c
+++ TCP/tcp_component.c
@@ -3,7 +3,7 @@ FROM_NET >> CheckConnection@NIC;
 CheckConnection@NIC (known) >> KnownConnection@NIC (send_ack) >> TO_NET;
 
 // common-case receive
-KnownConnection@NIC (in_order) >> (in) InOrder@NIC (to_app) >> (enqueueRx) MainQueue (dequeueRx) 
+KnownConnection@NIC (in_order) >> (in) InOrder@NIC (to_app) >> (nic_put) MainQueue (app_get) 
   <> recvInorder@API;
 
 // out-of-order
@@ -19,11 +19,11 @@ send@API >> (app_put) MainQueue (nic_get) >> CreateResponse@NIC >> TO_NET;
 new_connecion@API >> (client_request) NewConnection@APP_CORE >> (app_put) ContextQueue (kernel_get)
   <> KernelEntry@KERNEL >> (client_new_connection) ConnectionManager@KERNEL >> (kernel_put) ExceptionQueue (nic_get)
   >> Classifier@NIC >> (hs_syn) HandShake@NIC >> TO_NET;
-FROM_NET >> CheckConnection@NIC (unknown) >> UnknwonConnection@NIC >> (nic_put) ExceptionQueue (kernel_get)
+FROM_NET >> CheckConnection@NIC (unknown) >> UnknownConnection@NIC >> (nic_put) ExceptionQueue (kernel_get)
   <> KernelEntry@KERNEL >> (hs_syn_ack) ConnectionManager@KERNEL >> (kernel_put) ExceptionQueue (nic_get)
   >> Classifier@NIC >> (hs_ack) HandShake@NIC >> TO_NET;
 (hs_syn_ack) ConnectionManager@KERNEL >> (kernel_put) ContextQueue (app_get)
-  <> NewConnection@APP_CORE (check_clien_request) <> new_connection_ready@API;
+  <> NewConnection@APP_CORE (check_client_request) <> new_connection_ready@API;
 
 // new connection (server)
 listen@API >> (server_listen) NewConnection@APP_CORE >> (app_put) ContextQueue (kernel_get)
@@ -35,12 +35,17 @@ ExceptionQueue (kernel_get)
   <> KernelEntry@KERNEL >> (hs_ack) ConnectionManager@KERNEL >> (kernel_put) ContextQueue (app_get)
   <> NewConnection@APP_CORE (check_and_accept) <> check_and_accept@API;
 
-// new application thead???
+// new application thead
 init_NIC_interface@API >> (init_worker) APPManager@APP_CORE >> ContextQueue??? ;
 // There is no ContextQueue between this worker thread and kernel yet. How do they communicate?
   
 // congestion control
 
 // epoll
+int epoll_create(int size) 
+int epoll_create1() 
+epoll_ctl // register EPOLLIN and EPOLLOUT on all payload buffer
+epoll_wait
+epoll_pwait
   
 // Seem like we need global state for TCP because many action require flow state
