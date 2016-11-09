4767cb46c55ce1730409e990e9a3bdc5f56c5f5e
Mangpo Phothilimthana <mangpo@Mangpos-MacBook-Pro.local>
2016-11-08 18:01:13 -0800

    more code

---
+++ TCP/tcp_component.c
@@ -0,0 +1,46 @@
+// main
+FROM_NET >> CheckConnection@NIC;
+CheckConnection@NIC (known) >> KnownConnection@NIC (send_ack) >> TO_NET;
+
+// common-case receive
+KnownConnection@NIC (in_order) >> (in) InOrder@NIC (to_app) >> (enqueueRx) MainQueue (dequeueRx) 
+  <> recvInorder@API;
+
+// out-of-order
+KnownConnection@NIC (out_of_order) >> (in) OutOfOrder@NIC >> (nic_put) ExceptionQueue (kernel_get) 
+  <> KernelEntry@KERNEL (OOO) >> (in) OOO@KERNEL >> (kernel_put) ContextQueue (app_get) <> recvOOO@API;
+(in) OOO@KERNEL >> (kernel_put) ExceptionQUEUE (nic_get) 
+  >> Classifier@NIC (fix_ooo) >> UpdateFlow@NIC;
+
+// common-case send
+send@API >> (app_put) MainQueue (nic_get) >> CreateResponse@NIC >> TO_NET;
+
+// new connection (client)
+new_connecion@API >> (client_request) NewConnection@APP_CORE >> (app_put) ContextQueue (kernel_get)
+  <> KernelEntry@KERNEL >> (client_new_connection) ConnectionManager@KERNEL >> (kernel_put) ExceptionQueue (nic_get)
+  >> Classifier@NIC >> (hs_syn) HandShake@NIC >> TO_NET;
+FROM_NET >> CheckConnection@NIC (unknown) >> UnknwonConnection@NIC >> (nic_put) ExceptionQueue (kernel_get)
+  <> KernelEntry@KERNEL >> (hs_syn_ack) ConnectionManager@KERNEL >> (kernel_put) ExceptionQueue (nic_get)
+  >> Classifier@NIC >> (hs_ack) HandShake@NIC >> TO_NET;
+(hs_syn_ack) ConnectionManager@KERNEL >> (kernel_put) ContextQueue (app_get)
+  <> NewConnection@APP_CORE (check_clien_request) <> new_connection_ready@API;
+
+// new connection (server)
+listen@API >> (server_listen) NewConnection@APP_CORE >> (app_put) ContextQueue (kernel_get)
+  <> KernelEntry@KERNEL >> (server_listen) ConnectionManager@KERNEL;
+ExceptionQueue (kernel_get)
+  <> KernelEntry@KERNEL >> (hs_syn) ConnectionManager@KERNEL >> (kernel_put) ExceptionQueue (nic_get)
+  >> Classifier@NIC >> (hs_syn_ack) HandShake@NIC >> TO_NET;
+ExceptionQueue (kernel_get)
+  <> KernelEntry@KERNEL >> (hs_ack) ConnectionManager@KERNEL >> (kernel_put) ContextQueue (app_get)
+  <> NewConnection@APP_CORE (check_and_accept) <> check_and_accept@API;
+
+// new application thead???
+init_NIC_interface@API >> (init_worker) APPManager@APP_CORE >> ContextQueue??? ;
+// There is no ContextQueue between this worker thread and kernel yet. How do they communicate?
+  
+// congestion control
+
+// epoll
+  
+// Seem like we need global state for TCP because many action require flow state
--- memcached/memcached_component.c
+++ memcached/memcached_component.c
@@ -1,20 +1,37 @@
 FROM_NET >> (in) CheckHeader@NIC
 
 // get request
-CheckHeader (get) >> GetRequest@NIC (queue_insert) >> (put) SteeringQueue (get) <> app_loop@APP_CORE >> (lookup) KVS@APP_CORE (add_queue) >> (in) OutQueue (get_resp) >> GetResponse@NIC >> TO_NET;
-(lookup) KVS@APP_CORE (add_value) >> ValueBuffer (get) <> (get_value) GetResponse@NIC;
+CheckHeader (get) >> (rx) GetRequest@NIC (to_cpu) >> (put) SteeringQueue (get) 
+  <> app_loop@APP_CORE >> (lookup) KVS@APP_CORE (add_queue) >> (in) OutQueue 
+  >> classify >> (tx) GetRequest@NIC (to_net) >> TO_NET;
+(tx) GetRequest@NIC (DMA_read) >< (read) DMARead;
 
 // set request
-CheckHeader (set) >> (insert) LogManager@NIC >> (put) SteeringQueue (get) <> app_loop@APP_CORE >> (store) KVS@APP_CORE >> (in) OutQueue (get_resp) >> SetResponse@NIC >> TO_NET;
-app_loop@APP_CORE >> (full) LogManager@APP_CORE (add_queue) >> (in) OutQueue >> (new_seg) LogManager@NIC;
+CheckHeader (set) >> (rx) SetRequest@NIC (to_cpu) >> (put) SteeringQueue (get) 
+  <> app_loop@APP_CORE >> (store) KVS@APP_CORE (add_queue) >> (in) OutQueue 
+  >> classify >> (tx) SetRequest@NIC (to_net) >> TO_NET;
+(rx) SetRequest@NIC (DMA_write) >< (write) DMAWrite; // not very nice
+
+// full segment
+(rx) SetRequest@NIC (to_cpu) >> (put) SteeringQueue (get) 
+  <> app_loop@APP_CORE >> (full) LogManager@APP_CORE (add_queue) >> (in) OutQueue 
+  >> classify >> (new_seg) SetRequest@NIC;
 
 // unknown
-CheckHeader (except) >> Exception@NIC;
+(in) CheckHeader@NIC (error) >> SlowPath@NIC;
+(in) Classify@NIC (error) >> Exception@NIC >> TO_NET;
+
+// API
+process@API >> (run) app_loop@APP_CORE; // What is the semantics of >> (run)?
+garbage_collec@API >> (collect) LogManager@APP_CORE;
+// When using @API, programmers bind the computation units to the provided thread. For single-thread configuration, when @API connects to @APP_CORE, all called and posted actions @APP_CORE will be executed on that thread. The API call then returns when all computations are executed.
+// For multi-thread configuration, the API call returns whenever the main thread has no more work to do. Essentially, it only guarantees to finish the entry action. 
+
 
 Component CheckHeader@NIC {
   requires void get(packet p, uint32_t key_hash, uint6_t core_id);
   requires void set(packet p, uint32_t key_hash, uint6_t core_id);
-  requires void except(packet p, uint32_t key_hash);
+  requires void error(packet p, uint32_t key_hash);
   provides void in(packet p) {
     uint1_t pass = 1;
     // check header
@@ -25,14 +42,31 @@ Component CheckHeader@NIC {
       uint6_t core_id = reta[eqe.key_hash & 0xff]; 
       if(p.opcode == OP_GET) get(p, key_hash, core_id);
       else set(p, key_hash, core_id);
-    } else { except(p); }
+    } else { error(p); }
+  }
+}
+
+Component Classify@NIC {
+  
+  requires void get(cqe_entry e);
+  requires void set(cqe_entry e);
+  requires void new_weg(cqe_entry e);
+  requires void error(cqe_entry e);
+  provides void in(cqe_entry e) {
+    switch(e.type) {
+      case CQE_TYPE_GET: post get(e); break;
+      case CQE_TYPE_SET: post store(e); break;
+      case CQE_SEG_REGISTER: post new_seg(e); break;
+      case CQE_TYPE_ERROR: post error(e); break;
+    }
   }
 }
 
 ///////////////////// get @NIC ///////////////////////
 Component GetRequest@NIC {
-  requires void queue_insert(eqe_entry p, uint16_t size);
-  provides void get(packet p, uint32_t key_hash, uint6_t core_id) {
+  Channel {
+  requires void to_cpu(eqe_entry p, uint16_t size);
+  provides void rx(packet p, uint32_t key_hash, uint6_t core_id) {
         eqe_rx_get eqe; // do we have to worry about stack dellocation after return?
         eqe = cast<eqe_rx_get>(p); // copy common fields
         //eqe.src_port = p.src_port; 
@@ -43,6 +77,15 @@ Component GetRequest@NIC {
         eqe.keylen = p.keylen;
         uint16_t len = sizeof(eqe) + p.keylen;
         post queue_insert(eqe, len, core_id);
+  }
+  }
+  
+  Channel {
+    requires void to_net(packet_out p);
+    provides void tx(cqe_entry e) {
+      packet_out p = format_GET_response(call DMA_read(e.i, e.i_len), e.client); // TODO
+      post to_net(p);
+    }
   }
 }
 
@@ -56,19 +99,19 @@ struct log_region {
   lseg segments[M];
 }
 
-Component LogManager@NIC {
+Component SetRequest@NIC {
   log_region regions[N];
 
  Channel {
   requires void queue_insert(eqe_entry p, uint16_t size);
-  require void write_DMA(size_t addr, size_t size, entry val);
+  require void DMA_write(size_t addr, size_t size, entry val);
 
-  provides void insert(packet p, uint32_t key_hash, uint6_t core_id) {
+  provides void rx(packet p, uint32_t key_hash, uint6_t core_id) {
     log_region region = regions[core_id];
     int addr = item_allocation(region, p.keylen + p.vallen);
     item i;
     // ...
-    call DMAWrite(addr, sizeof(item + p.keylen + p.vallen), i); // call
+    call DMA_write(addr, sizeof(item + p.keylen + p.vallen), i); // call
     eqe_rx_set eqe;
     eqe = cast<eqe_rx_get>(p);
     eqe.type = EQE_TYPE_GET;
@@ -76,7 +119,7 @@ Component LogManager@NIC {
     uint16_t len = sizeof(eqe);
     post queue_insert(eqe, len, core_id); // post
   }
-
+  
   int item_allocation(log_region region, int size) {
     int len = size + sizeof(item);
     lseg active = region.segments[active_seg];
@@ -93,19 +136,40 @@ Component LogManager@NIC {
     return addr;
   }
  }
+ 
+ Channel {
+   requires void to_net(packet_out p);
+   provides void tx(cqe_entry e) {
+     packet_out p = format_SET_response(e.client) // TODO
+     post to_net(p);
+   }
+ }
+
 
  Channel {
-  provides void new_seg(int index, lseg seg) { regions[index] = seg; }
+   provides void new_seg(cqe_entry e) {
+     regions[e.index] = e.seg; 
+   }
  }
 }
 
-///////////////////// Descriptor Queue ///////////////////////
+///////////////////// Queue ///////////////////////
 Composite SteeringQueue {
   provides void put(eqe_entry p, size_t size, uint6_t core_id);
   provides eqe_entry get();
 
   Implementation {
     (this.put) == (enqueueRx) NICQueue@NIC (write_DMA) >> (write) DMAWrite (read) <> (read) APPQueue@APP_CORE (dequeueRx) == (this.get);
+  }
+}
+
+Composite OutQueue {
+  provides void in(cqe_entry p, size_t size, uint6_t core_id);
+  requires void out(cqe_entry e);
+  
+  Implementation {
+    (this.in) == (enqueueTX) APPQueue@APP_CORE (doorbell_write) >> (in) Doorbell (out) >> (dequeueTX) NICQueue@NIC (out) == (this.out);
+    (enqueueTX) APPQueue@APP_Core (mem_write) >< (write) DMARead (read) <> (DMA_read) NICQueue@NIC (dequeueTX);
   }
 }
 
@@ -120,6 +184,11 @@ Component DMAWrite {
   provides entry read(size_t addr); // @APP_CORE, should this return an struct instance or a pointer?
 }
 
+Component DMARead {
+  provides void write(size_t addr, size_t size, entry val); // @APP_CORE
+  provides entry read(size_t addr); // @NIC
+}
+
 Component NICQueue@NIC {
   queue cores_rx[N];
 
@@ -128,7 +197,7 @@ Component NICQueue@NIC {
     provides void enqueueRX(eqe_entry p, size_t size, uint6_t core_id) {
       core_r core = cores_rx[core_id];
     
-      if(core's queue is not full) {
+      if(cores queue is not full) {
         post write_DMA(core.head, size, eqe); // pointer
         // core.tail = ;
       }
@@ -143,19 +212,19 @@ Component NICQueue@NIC {
       ...
       if(db.rx_head != -1)
         cores_rx[db.core_id].head = db.rx_head;
-      entry e = read(db.addr);
+      entry e = call DMA_read(db.addr);
       post out(e);
     }
   }
 }
 
 Component APPQueue@APP_CORE {
-  core_r core;
+  core_r core_rx, core_tx;
 
   Channel {
     requires entry read(size_t addr);
     provides eqe_entry dequeueRX() {
-      eqe_entry e =  read(core.head);
+      eqe_entry e =  read(core_rx.start + core_rx.head);
       if(e.owner_bit) {
         e.owner_bit = 0;
         // core.head = ;
@@ -165,6 +234,104 @@ Component APPQueue@APP_CORE {
   }
 
   Channel {
-    provides void enqueueTX(entry e);
+    requries void doorbell_write(doorbell bd);
+    requires void mem_write(size_t addr, size_t size, entry val);
+    provides void enqueueTX(entry e) {
+      // adjust core.head
+      call mem_write(core_tx.start + core_tx.head, ...);
+      doorbell db;
+      db.addr = core_tx.start + core_tx.head;
+      db.rx_head = core.rx_head;
+      post doorbell_write(db);
+    }
   }
 }
+
+///////////////////// APP ///////////////////////
+// <> app_loop@APP_CORE >> (lookup) KVS@APP_CORE (add_queue) >> (in) OutQueue (get_resp) >> GetResponse@NIC >> TO_NET;
+// DMARead (read) <> (read) GetResponse@NIC;
+
+Component app_loop@APP_CORE {
+  requires eqe_entry get();
+  requires void lookup(eqe_entry e);
+  requires void store(eqe_entry e);
+  requires void full(eqe_entry e);
+  provides void run() {
+    eqe_entry e = get();
+    switch(e.type) {
+      case EQE_TYPE_GET: post lookup(e); break;
+      case EQE_TYPE_SET: post store(e); break;
+      case EQE_FULL_SEG: post full(e); break;
+    }
+  }
+}
+
+Component KVS@APP_CORE {
+  requires void add_queue(cqe_entry e);
+  
+  void add_GET_txqueue(item i, uint32_t client) {
+    // e = create cqe_send_getresponse
+    post add_queue(e);
+  }
+  
+  void add_FAIL_txqueue(uint32_t client) {
+    // e = create cqe_send_error
+    post add_queue(e);
+  }
+  
+  void add_SET_txqueue(uint32_t client) {
+    // e = create cqe_send_setresponse
+    post add_queue(e);
+  }
+  
+  provides void lookup(eqe_entry e) {
+    item i = hash_table_lookup(e.hash, e.key);
+    if(i)
+      add_GET_txqueue(i, e.client);
+    else
+      add_FAIL_txqueue(e.client);
+  }
+  
+  provides void store(eqe_entry e) {
+    hash_insert(e.hash, e.item)
+    add_SET_txqueue(e.client)
+  }
+
+}
+
+Component LogManager@APP_CORE {
+  lseg unused_segments[L]; // per core
+  
+  // Register new log segment to NIC
+  Channel {
+    requires void add_queue(cqe_entry e);
+  
+    void add_SEG_REGISTER(lseg seg) {
+      // e = create CQE_SEG_REGISTER
+      post add_equeue(e);
+    }
+  
+    provides void full(eqe_entry e) {
+      gc_register_full_segment(e.base);
+      add_SEG_REGISTER(get_unused_segment());
+    }
+  }
+  
+  // Garbage collcection
+  Channel {
+    void clear_segment(lseg) {
+      for i in live_segments(lseg):
+        move_to_new_segment(i)
+      unused_segments += lseg
+    }
+    
+    provides void collect() {
+      for lseg in log segments with live items:
+        stats = scan_segment(lseg)
+        if stats.free / stats.total < THRESHOLD:
+          clear_segment(lseg)
+    }
+  }
+}
+
+
