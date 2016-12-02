8a6537cd96f71ef53a1eb2d2918b52fa761dd711
Mangpo Phothilimthana <mangpo@cs.washington.edu>
2016-12-02 12:01:10 -0800

    simple programs are working. need mroe test

--- compiler/test2.py
+++ compiler/test2.py
@@ -1,62 +1,168 @@
 from ast import Element, Port
 import re
 
+var_id = 0
+def fresh_var_name():
+    global var_id
+    name = '_x' + str(var_id)
+    var_id += 1
+    return name
+
+def check_no_args(args):
+    if not(args == ""):
+        raise Exception("Cannot pass an argument when retrieving data from an input port.")
+
+"""Return the last charactor before s[i] that is not a space, along with its index.
+s -- string
+i -- index
+"""
+def last_non_space(s,i):
+    i -= 1
+    while i >= 0 and s[i] == ' ':
+        i -= 1
+
+    if i >= 0:
+        return (s[i],i)
+    else:
+        return (None,-1)
+
+"""Return the first charactor start from s[i] that is not a space, along with its index.
+
+s -- string
+i -- index
+"""
+def first_non_space(s,i):
+    l = len(s)
+    while i < l and s[i] == ' ':
+        i += 1
+
+    if i < l:
+        return (s[i],i)
+    else:
+        return (None,-1)
+
+"""Remove the reading from port statment from src, 
+and put its LHS of the statement in port2args.
+
+src       -- string source code
+port2args -- map port name to a list of (type,argument name)
+port      -- port name
+p_eq      -- position of '=' of the statement to be removed
+p_end     -- the ending positiion of the statement to be removed (after ';')
+inport_types -- data types of the port
+"""
+def remove_asgn_stmt(src,port2args,port,p_eq, p_end, inport_types):
+    p_start = max(0,src[:p_eq].rfind(';'))
+    if src[p_start] == ';':
+        p_start += 1
+
+    decl = src[p_start:p_eq].lstrip().rstrip().lstrip("(").rstrip(")").lstrip().rstrip()
+    args = decl.split(",")
+    port2args[port] = args
+    argtypes = [x.split()[0] for x in args]
+
+    if not(argtypes == inport_types):
+        raise Exception("Argument types mismatch at an input port '%s'." % port)
+        
+    return src[:p_start] + src[p_end:]
+
+"""Remove the reading from port statment from src, 
+when the reading is not saved in any variable or used in any expression.
+
+src       -- string source code
+port2args -- map port name to a list of (type,argument name)
+port      -- port name
+p_start   -- the starting position of the statement to be removed
+             (after previous ';' or 0 if no previous ';')
+p_end     -- the ending positiion of the statement to be removed (after ';')
+inport_types -- data types of the port
+"""
+def remove_nonasgn_stmt(src,port2args,port,p_start, p_end, inport_types):
+    args = [t + ' ' + fresh_var_name() for t in inport_types]
+    port2args[port] = args
+    return src[:p_start] + src[p_end:]
+    
+"""Remove the reading from port expression from src, 
+and replace it with a fresh variable name.
+
+src       -- string source code
+port2args -- map port name to a list of (type,argument name)
+port      -- port name
+p_start   -- the starting position of the expression to be removed
+p_end     -- the ending positiion of the statement to be removed (after ')')
+inport_types -- data types of the port
+"""
+def remove_expr(src,port2args,port,p_start, p_end, inport_types):
+    n = len(inport_types)
+    if n > 1 or n == 0:
+        raise Exception("Input port '%s' returns %d values. It cannot be used as an expression." % (n,port))
+
+    name = fresh_var_name()
+    port2args[port] = [inport_types[0] + ' ' + name]
+    return src[:p_start] + name + src[p_end:]
+
+
 def element_to_function(src, funcname, inports, output2func):
     port2args = {}
-    for f in inports:
+    for portinfo in inports:
+        port = portinfo.name
+        argtypes = portinfo.argtypes
         match = False
         index = 0
         while not(match):
-            p = src.find(f,index)
-            #m = re.search(';([^=]+)=[ ]*' + funcname + '[ ]*\(([^\)]*)\)[ ]*;',src)
-            if p == -1:
-                raise Exception("Element '%s' never get data from input port '%s'." % (funcname,f))
+            m = re.search(port + '[ ]*\(([^\)]*)\)',src[index:])
+            if m == None:
+                raise Exception("Element '%s' never get data from input port '%s'." % (funcname,port))
+            p = m.start(0)
             if p == 0 or re.search('[^a-zA-Z0-9_]',src[p-1]):
-                last = max(0,src[:p].rfind(";"))
-                next = src.find(";",p)
-                stmt = src[last+1:next+1]
-                src = src[:last+1] + src[next+1:]
-                
-                eq = stmt.find("=")
-                if eq >= 0:
-                    args_begin = stmt.find("(",eq)
-                    args_end = stmt.find(")",eq)
-                    args = stmt[args_begin+1:args_end].lstrip().rstrip()
-                    if not(args == ""):
-                        raise Exception("Cannot pass an argument when retrieving data from an input port.")
-                    decl = stmt[:eq].lstrip().rstrip().lstrip("(").rstrip(")").lstrip().rstrip()
-                    port2args[f] = decl.split(",")
+                check_no_args(m.group(1))
+                c1, p1 = last_non_space(src,p)
+                c2, p2 = first_non_space(src,m.end(0))
+
+                if c1 == '=' and c2 == ';':
+                    src = remove_asgn_stmt(src,port2args,port,p1,p2+1,argtypes)
+                elif (c1 == ';' or c1 == None) and c2 == ';':
+                    src = remove_nonasgn_stmt(src,port2args,port,p1+1,p2+1,argtypes)
                 else:
-                    port2args[f] = []
+                    src = remove_expr(src,port2args,port,p,m.end(0),argtypes)
+
                 match = True
             else:
-                index = p + 1
+                index = p+1
                 
-        p = src.find(f,index)
-        if p >= 0:
+        m = re.search(port + '[ ]*\(([^\)]*)\)',src[index:])
+        if m and re.search('[^a-zA-Z0-9_]',src[m.start(0)-1]):
             raise Exception("Cannot get data from input port '%s' more than one time in element '%s'." % (f, funcname))
 
     for o in output2func:
-        p = src.find(o + "(")
-        if p == 0 or src[p-1] == " " or src[p-1] == "{" or src[p-1] == ";":
+        m = re.search(o + '[ ]*\(',src[index:])
+        if m == None:
+            raise Exception("Element '%s' never send data from output port '%s'." % (funcname,o))
+        p = m.start(0)
+        if p == 0 or re.search('[^a-zA-Z0-9_]',src[p-1]):
             src = src[:p] + output2func[o] + src[p+len(o):]
 
     args = []
     for port in inports:
-        args = args + port2args[port]
+        args = args + port2args[port.name]
 
     src = "void " + funcname + "(" + ", ".join(args) + ") {" + src + "}"
     print src
     return src
+
+def test_element(e):
+    element_to_function(e.code, e.name, e.inports,
+                        dict([(x.name, x.name + '_func') for x in e.outports]))
+    
                                                               
 
 def test1():
     src = r'''
-  int i = in1();
+  int i = in();
   i = i + i;
   out(i+1);
 '''
-    element_to_function(src, "f", ["in1"], {"out": "g"})
+    element_to_function(src, "f", [Port("in", ["int"])], {"out": "g"})
 
 def test2():
     e1 = Element("Power",
@@ -72,9 +178,21 @@ def test2():
                 r'''
                 out(in()+1);
                 ''')
-    element_to_function(e1.code, e1.name, [x.name for x in e1.inports],
-                        dict([(x.name, x.name + '_func') for x in e1.outports]))
-    element_to_function(e2.code, e2.name, [x.name for x in e2.inports],
-                        dict([(x.name, x.name + '_func') for x in e2.outports])) #: TODO: currently broken
+    e3 = Element("AddSub",
+                [Port("in1", ["int"]), Port("in2", ["int"])],
+                [Port("add_out", ["int"]), Port("sub_out", ["int"])], 
+                r'''
+  int a = in1(); int b = in2();
+  add_out(a+b); sub_out(a-b);
+                ''')
+    e4 = Element("Print",
+                [Port("in", [])],
+                [Port("out", [])], 
+                r'''in(); printf("hello\n"); out();
+                ''')
+    test_element(e1)
+    test_element(e2)
+    test_element(e3)
+    test_element(e4)
 
 test2()
