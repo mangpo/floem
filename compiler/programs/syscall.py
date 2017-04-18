7c5bbea91221813c5c54c8419d001f8e25dc3ede
mangpo <mangpo@eecs.berkeley.edu>
2017-04-18 02:39:19 -0700

    use higher function to create syscall

--- compiler/programs/syscall.py
+++ compiler/programs/syscall.py
@@ -3,7 +3,7 @@ from elements_library import *
 from queue import *
 
 @composite_instance("syscall")
-def syscall(arg_app, ret_kernel, t_app, t_ker):
+def syscall_func(arg_app, ret_kernel, t_app, t_ker):
     app2ker_enq, app2ker_deq = create_circular_queue_instances("app2ker", "int", 8, blocking=True)
     app2ker_enq(arg_app)
     arg_ker = app2ker_deq()
@@ -25,17 +25,35 @@ f = Forward("f")
 inc = Inc()
 p = Print()
 
-t_app = API_thread("add_syscall", ["int"], None)
-t_ker = internal_thread("kernel")
-t_app.run(f, p)
-t_ker.run(inc)
+# Use before definition. This is quite ugly.
+# t_app = API_thread("add_syscall", ["int"], None)
+# t_ker = internal_thread("kernel")
+# t_app.run(f, p)
+# t_ker.run(inc)
+#
+# arg_app2 = f(None)
+# ret_ker2 = inc(None)
+# arg_ker2, ret_app2 = syscall_func(arg_app2, ret_ker2, t_app, t_ker)
+# inc(arg_ker2)
+# p(ret_app2)
 
-# TODO: use before definition. This is quite ugly.
-arg_app2 = f(None)
-ret_ker2 = inc(None)
-arg_ker2, ret_app2 = syscall(arg_app2, ret_ker2, t_app, t_ker)
-inc(arg_ker2)
-p(ret_app2)
+def syscall(ker_func, t_app):
+    def dummy(arg_app2):
+        t_ker = internal_thread("kernel")
+        t_ker.run(ker_func)
+
+        ret_ker2 = ker_func()
+        arg_ker2, ret_app2 = syscall_func(arg_app2, ret_ker2, t_app, t_ker)
+        ker_func(arg_ker2)
+        return ret_app2
+    return dummy
+
+t_app = API_thread("add_syscall", ["int"], None)
+t_app.run(f, p)
+kernel_add = syscall(inc, t_app)
+
+p(kernel_add(f(None)))
+
 
 c = Compiler()
 c.testing = "f(0); f(1); f(2);"
