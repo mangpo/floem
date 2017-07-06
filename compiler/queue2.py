4362cbdba05edb9d84f2bbb314236ef5693df302
mangpo <mangpo@eecs.berkeley.edu>
2017-07-06 11:35:02 -0700

    cavium: queue_custom_owner_bit. not working yet

--- compiler/queue2.py
+++ compiler/queue2.py
@@ -248,7 +248,12 @@ def queue_variable_size(name, size, n_cores, blocking=False, enq_atomic=False, d
 
     return EnqueueAlloc, EnqueueSubmit, DequeueGet, DequeueRelease, ScanClean if scan else None
 
-
+# TODO
+# 1. ScanRelease
+# 2. Dequeue & Scan: output(type*, uintptr_t)
+# 3. TxRelease & ScanRelease: input(type*, uintptr_t)
+# 4. Cavium: sync, dma_read_with_buf
+# 5. Make storm works again
 def queue_custom_owner_bit(name, type, size, n_cores, owner,
                            blocking=False, enq_atomic=False, deq_atomic=False, scan=False, enq_output=False):
     """
@@ -281,16 +286,43 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
     }
     ''' % ('%', size, '%', size)
 
+    atomic_src_cvm = r'''
+        size_t old = p->offset;
+        size_t new = (old + 1) %s %d;
+        while(!__sync_bool_compare_and_swap(&p->offset, old, new)) {
+            old = p->offset;
+            new = (old + 1) %s %d;
+        }
+        ''' % ('%', size, '%', size)  # TODO: __sync_bool_compare_and_swap
+
     wait_then_copy = r'''
     while(q->data[old].%s != 0) __sync_synchronize();
     rte_memcpy(&q->data[old], x, sizeof(%s));
     __sync_synchronize();
     ''' % (owner, type)
 
+    init_read_cvm = r'''
+        uintptr_t addr = &q->data[old];
+        %s* entry;
+        int size = sizeof(%s);
+        dma_read(addr, size, &entry);
+        ''' % (type, type)
+
+    wait_then_copy_cvm = init_read_cvm + r'''
+        while(entry->%s) dma_read_with_buff(addr, size, &entry);
+        memcpy(entry, x, size);
+        dma_write(addr, size, entry);
+        ''' % (owner)  # TODO: dma_read_with_buff
+
     wait_then_get = r'''
     while(q->data[old].%s == 0) __sync_synchronize();
     %s x = &q->data[old];
     ''' % (owner, type_star)
+
+    wait_then_get_cvm = init_read_cvm + r'''
+        while(entry->%s == 0) dma_read_with_buff(addr, size, &entry);
+        %s* x = entry;
+        ''' % (owner, type)
 
     inc_offset = "p->offset = (p->offset + 1) %s %d;\n" % ('%', size)
 
@@ -316,7 +348,7 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
 
             block_noatom = "size_t old = p->offset;\n" + wait_then_copy + inc_offset
 
-            block_atom = atomic_src + wait_then_copy
+            block_atom = atomic_src_cvm + wait_then_copy
 
             if blocking:
                 src = block_atom if enq_atomic else block_noatom
@@ -335,6 +367,36 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
             ''' % (type_star, Storage.__name__)
                        + src + out_src)
 
+        def impl_cavium(self):
+            noblock_noatom = "size_t old = p->offset;\n" + init_read_cvm + r'''
+                if(entry->%s == 0) {
+                    memcpy(entry, x, size);
+                    dma_write(addr, size, entry);
+                    p->offset = (p->offset + 1) %s %d;
+                }
+                ''' % (owner, '%', size)
+
+            block_noatom = "size_t old = p->offset;\n" + wait_then_copy_cvm + inc_offset
+
+            block_atom = atomic_src_cvm + wait_then_copy_cvm
+
+            if blocking:
+                src = block_atom if enq_atomic else block_noatom
+            else:
+                if enq_atomic:
+                    raise Exception("Unimplemented for non-blocking but atomic.")
+                else:
+                    src = noblock_noatom
+
+            out_src = "dma_free(entry); \noutput { out(x); }\n" if enq_output else 'dma_free(entry);\n'
+
+            self.run_c(r'''
+            (%s x, size_t c) = inp();
+            circular_queue* p = this->cores[c];
+            %s* q = p->queue;
+            ''' % (type_star, Storage.__name__)
+                       + src + out_src)
+
     class Dequeue(Element):
         this = Persistent(deq_all.__class__)
 
@@ -342,7 +404,7 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
 
         def configure(self):
             self.inp = Input(Size)
-            self.out = Output(type_star)
+            self.out = Output(type_star, 'uintptr_t')
 
         def impl(self):
             noblock_noatom = r'''
@@ -375,17 +437,60 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
                         ''' % Storage.__name__
                        #+ debug
                        + src
-                       + "output { out(x); }\n")
+                       + "output { out(x, 0); }\n")
+
+        def impl_cavium(self):
+            noblock_noatom = "size_t old = p->offset;\n" + init_read_cvm + r'''
+            %s x = NULL;
+            if(entry->%s != 0) {
+                x = entry;
+                p->offset = (p->offset + 1) %s %d;
+            }
+            ''' % (type_star, owner, '%', size)
+
+            block_noatom = "size_t old = p->offset;\n" + wait_then_get_cvm + inc_offset
+
+            block_atom = atomic_src_cvm + wait_then_get_cvm
+
+            if blocking:
+                src = block_atom if deq_atomic else block_noatom
+            else:
+                if deq_atomic:
+                    raise Exception("Unimplemented for non-blocking but atomic.")
+                else:
+                    src = noblock_noatom
+
+            debug = r'''printf("deq %ld\n", c);'''
+
+            self.run_c(r'''
+                        (size_t c) = inp();
+                        circular_queue* p = this->cores[c];
+                        %s* q = p->queue;
+                        ''' % Storage.__name__
+                       #+ debug
+                       + src
+                       + "output { out(x, addr); }\n")
 
     class Release(Element):
         def configure(self):
-            self.inp = Input(type_star)
+            self.inp = Input(type_star, 'uintptr_t')
 
         def impl(self):
             self.run_c(r'''
-            (%s x) = inp();
+            (%s x, uintptr_t addr) = inp();
             if(x) x->%s = 0;
             ''' % (type_star, owner))
+
+        def impl_cavium(self):
+            self.run_c(r'''
+            (%s x, uintptr_t addr) = inp();
+            if(x) {
+                x->%s = 0;
+                dma_write(addr, size, x);
+                dma_free(x);
+            }
+            ''' % (type_star, owner))
+
 
     class Scan(Element):
         this = Persistent(enq_all.__class__) if scan == 'enq' else Persistent(deq_all.__class__)
@@ -395,7 +500,7 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
 
         def configure(self):
             self.inp = Input(Size)
-            self.out = Output(type_star)
+            self.out = Output(type_star, 'uintptr_t')
 
         def impl(self):
             self.run_c(r'''
@@ -411,15 +516,51 @@ def queue_custom_owner_bit(name, type, size, n_cores, owner,
             q->clean = (q->clean + 1) %s q->len;
         }
     }
-    output { out(entry); }
-            ''' % (type_star, type_star, owner, '%'))
+    output { out(entry, 0); }
+            ''' % (Storage.__name__, type, owner, '%'))
+
+        def impl_cavium(self):
+            self.run_c(r'''
+    (size_t c) = in_core();
+    circular_queue_scan *q = this->cores[c];
+    %s* p = q->queue;
+    %s* entry = NULL;
+    uintptr_t addr;
+    if (q->clean != q->offset) {
+        addr = &q->data[q->clean];
+        dma_read(addr, sizeof(%s), &entry);
+        if ((entry->%s) != 0) {
+            dma_free(entry);
+            entry = NULL;
+        } else {
+            q->clean = (q->clean + 1) %s q->len;
+        }
+    }
+    output { out(entry, addr); }
+            ''' % (Storage.__name__, type, type, owner, '%'))
+
+    class ScanRelease(Element):
+        def configure(self):
+            self.inp = Input(type_star, 'uintptr_t')
+
+        def impl(self):
+            self.run_c("")
+
+        def impl_cavium(self):
+            self.run_c(r'''
+            (%s x, uintptr_t addr) = inp();
+            if(x) {
+                dma_free(x);
+            }
+            ''' % (type_star, owner))
 
     Enqueue.__name__ = prefix + Enqueue.__name__
     Dequeue.__name__ = prefix + Dequeue.__name__
     Release.__name__ = prefix + Release.__name__
     Scan.__name__ = prefix + Scan.__name__
+    ScanRelease.__name__ = prefix + ScanRelease.__name__
 
-    return Enqueue, Dequeue, Release, Scan if scan else None
+    return Enqueue, Dequeue, Release, Scan if scan else None, ScanRelease if scan else None
 
 
 def queue_shared_head_tail(name, type, size, n_cores):
