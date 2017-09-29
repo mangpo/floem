0ba74bfa7edf5383b61390175a27770a0ef9d210
mangpo <mangpo@cs.washington.edu>
2017-09-29 01:27:50 +0000

    storm cpu: tx dequeue atomic=false

--- compiler/storm_queue/main_dccp.py
+++ compiler/storm_queue/main_dccp.py
@@ -666,7 +666,7 @@ rx_enq_creator, rx_deq_creator, rx_release_creator = \
 
 tx_enq_creator, tx_deq_creator, tx_release_creator = \
     queue2.queue_custom_owner_bit("tx_queue", "struct tuple", MAX_ELEMS, n_cores, "task",
-                                  enq_blocking=True, deq_atomic=True)
+                                  enq_blocking=True, deq_atomic=False)
 
 
 class RxState(State):
