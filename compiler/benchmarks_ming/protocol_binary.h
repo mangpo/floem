f7369e57e33d90328d27f2f9b27cd4c1e7c497cf
mangpo <mangpo@eecs.berkeley.edu>
2017-11-08 23:52:48 -0800

    ming's benchmarks: dpdk gen

---
+++ compiler/benchmarks_ming/protocol_binary.h
@@ -0,0 +1,249 @@
+#ifndef PROTOCOL_BINARY_H
+#define PROTOCOL_BINARY_H
+
+    #define UDP_HEADER_SIZE 8
+
+    typedef struct {
+      uint16_t req_id;
+      uint16_t seq_id;
+      uint16_t n_data;
+      uint16_t extras;
+    } memcached_udp_header;
+
+    /**
+     * Definition of the legal "magic" values used in a packet.
+     * See section 3.1 Magic byte
+     */
+    typedef enum {
+        PROTOCOL_BINARY_REQ = 0x80,
+        PROTOCOL_BINARY_RES = 0x81
+    } protocol_binary_magic;
+
+
+    /**
+     * Definition of the valid response status numbers.
+     * See section 3.2 Response Status
+     */
+    typedef enum {
+        PROTOCOL_BINARY_RESPONSE_SUCCESS = 0x00,
+        PROTOCOL_BINARY_RESPONSE_KEY_ENOENT = 0x01,
+        PROTOCOL_BINARY_RESPONSE_KEY_EEXISTS = 0x02,
+        PROTOCOL_BINARY_RESPONSE_E2BIG = 0x03,
+        PROTOCOL_BINARY_RESPONSE_EINVAL = 0x04,
+        PROTOCOL_BINARY_RESPONSE_NOT_STORED = 0x05,
+        PROTOCOL_BINARY_RESPONSE_DELTA_BADVAL = 0x06,
+        PROTOCOL_BINARY_RESPONSE_AUTH_ERROR = 0x20,
+        PROTOCOL_BINARY_RESPONSE_AUTH_CONTINUE = 0x21,
+        PROTOCOL_BINARY_RESPONSE_UNKNOWN_COMMAND = 0x81,
+        PROTOCOL_BINARY_RESPONSE_ENOMEM = 0x82
+    } protocol_binary_response_status;
+
+
+        /**
+     * Defintion of the different command opcodes.
+     * See section 3.3 Command Opcodes
+     */
+    typedef enum {
+        PROTOCOL_BINARY_CMD_GET = 0x00,
+        PROTOCOL_BINARY_CMD_SET = 0x01,
+        PROTOCOL_BINARY_CMD_ADD = 0x02,
+        PROTOCOL_BINARY_CMD_REPLACE = 0x03,
+        PROTOCOL_BINARY_CMD_DELETE = 0x04,
+        PROTOCOL_BINARY_CMD_INCREMENT = 0x05,
+        PROTOCOL_BINARY_CMD_DECREMENT = 0x06,
+        PROTOCOL_BINARY_CMD_QUIT = 0x07,
+        PROTOCOL_BINARY_CMD_FLUSH = 0x08,
+        PROTOCOL_BINARY_CMD_GETQ = 0x09,
+        PROTOCOL_BINARY_CMD_NOOP = 0x0a,
+        PROTOCOL_BINARY_CMD_VERSION = 0x0b,
+        PROTOCOL_BINARY_CMD_GETK = 0x0c,
+        PROTOCOL_BINARY_CMD_GETKQ = 0x0d,
+        PROTOCOL_BINARY_CMD_APPEND = 0x0e,
+        PROTOCOL_BINARY_CMD_PREPEND = 0x0f,
+        PROTOCOL_BINARY_CMD_STAT = 0x10,
+        PROTOCOL_BINARY_CMD_SETQ = 0x11,
+        PROTOCOL_BINARY_CMD_ADDQ = 0x12,
+        PROTOCOL_BINARY_CMD_REPLACEQ = 0x13,
+        PROTOCOL_BINARY_CMD_DELETEQ = 0x14,
+        PROTOCOL_BINARY_CMD_INCREMENTQ = 0x15,
+        PROTOCOL_BINARY_CMD_DECREMENTQ = 0x16,
+        PROTOCOL_BINARY_CMD_QUITQ = 0x17,
+        PROTOCOL_BINARY_CMD_FLUSHQ = 0x18,
+        PROTOCOL_BINARY_CMD_APPENDQ = 0x19,
+        PROTOCOL_BINARY_CMD_PREPENDQ = 0x1a,
+        PROTOCOL_BINARY_CMD_TOUCH = 0x1c,
+        PROTOCOL_BINARY_CMD_GAT = 0x1d,
+        PROTOCOL_BINARY_CMD_GATQ = 0x1e,
+        PROTOCOL_BINARY_CMD_GATK = 0x23,
+        PROTOCOL_BINARY_CMD_GATKQ = 0x24,
+        PROTOCOL_BINARY_CMD_SASL_LIST_MECHS = 0x20,
+        PROTOCOL_BINARY_CMD_SASL_AUTH = 0x21,
+        PROTOCOL_BINARY_CMD_SASL_STEP = 0x22,
+        /* These commands are used for range operations and exist within
+         * this header for use in other projects.  Range operations are
+         * not expected to be implemented in the memcached server itself.
+         */
+        PROTOCOL_BINARY_CMD_RGET      = 0x30,
+        PROTOCOL_BINARY_CMD_RSET      = 0x31,
+        PROTOCOL_BINARY_CMD_RSETQ     = 0x32,
+        PROTOCOL_BINARY_CMD_RAPPEND   = 0x33,
+        PROTOCOL_BINARY_CMD_RAPPENDQ  = 0x34,
+        PROTOCOL_BINARY_CMD_RPREPEND  = 0x35,
+        PROTOCOL_BINARY_CMD_RPREPENDQ = 0x36,
+        PROTOCOL_BINARY_CMD_RDELETE   = 0x37,
+        PROTOCOL_BINARY_CMD_RDELETEQ  = 0x38,
+        PROTOCOL_BINARY_CMD_RINCR     = 0x39,
+        PROTOCOL_BINARY_CMD_RINCRQ    = 0x3a,
+        PROTOCOL_BINARY_CMD_RDECR     = 0x3b,
+        PROTOCOL_BINARY_CMD_RDECRQ    = 0x3c
+        /* End Range operations */
+    } protocol_binary_command;
+
+    /**
+     * Definition of the data types in the packet
+     * See section 3.4 Data Types
+     */
+    typedef enum {
+        PROTOCOL_BINARY_RAW_BYTES = 0x00
+    } protocol_binary_datatypes;
+
+    /**
+     * Definition of the header structure for a request packet.
+     * See section 2
+     */
+    typedef union {
+        struct {
+            uint8_t magic;
+            uint8_t opcode;
+            uint16_t keylen;
+            uint8_t extlen;
+            uint8_t datatype;
+            uint16_t status;
+            uint32_t bodylen;
+            uint32_t opaque;
+            uint64_t cas;
+        } request;
+        uint8_t bytes[24];
+    } protocol_binary_request_header;
+
+
+#define ETH_ALEN     6
+
+/** Ethernet header */
+struct eth_addr {
+  uint8_t addr[ETH_ALEN];
+} __attribute__ ((packed));
+
+struct eth_hdr {
+#if ETH_PAD_SIZE
+  uint8_t padding[ETH_PAD_SIZE];
+#endif
+  struct eth_addr dest;
+  struct eth_addr src;
+  uint16_t type;
+} __attribute__ ((packed));
+
+struct ip_addr {
+  //uint32_t addr;
+  uint8_t addr[4];
+} __attribute__ ((packed));
+
+struct ip_hdr {
+  /* version / header length */
+  uint8_t _v_hl;
+  /* type of service */
+  uint8_t _tos;
+  /* total length */
+  uint16_t _len;
+  /* identification */
+  uint16_t _id;
+  /* fragment offset field */
+  uint16_t _offset;
+  /* time to live */
+  uint8_t _ttl;
+  /* protocol*/
+  uint8_t _proto;
+  /* checksum */
+  uint16_t _chksum;
+  /* source and destination IP addresses */
+  struct ip_addr src;
+  struct ip_addr dest;
+} __attribute__ ((packed));
+
+struct udp_hdr{
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint16_t len;
+    uint16_t cksum;
+} __attribute__((packed));
+
+/* Eternet protocal type. */
+#define ETHERTYPE_PUP    0x0200/* PUP protocol */
+#define ETHERTYPE_IPv4   0x0800/* IP protocol */
+#define ETHERTYPE_ARP    0x0806/* Addr. resolution protocol */
+
+/* ARP protocol opcodes. */
+#define	ARPOP_REQUEST	1		/* ARP request.  */
+#define	ARPOP_REPLY	2		/* ARP reply.  */
+#define	ARPOP_RREQUEST	3		/* RARP request.  */
+#define	ARPOP_RREPLY	4		/* RARP reply.  */
+#define	ARPOP_InREQUEST	8		/* InARP request.  */
+#define	ARPOP_InREPLY	9		/* InARP reply.  */
+#define	ARPOP_NAK	10		/* (ATM)ARP NAK.  */
+
+/* See RFC 826 for protocol description.  ARP packets are variable
+   in size; the arphdr structure defines the fixed-length portion.
+   Protocol type values are the same as those for 10 Mb/s Ethernet.
+   It is followed by the variable-sized fields ar_sha, arp_spa,
+   arp_tha and arp_tpa in that order, according to the lengths
+   specified.  Field names used correspond to RFC 826.  */
+
+struct arp_hdr
+  {
+    unsigned short int arp_hrd;		/* Format of hardware address.  */
+    unsigned short int arp_pro;		/* Format of protocol address.  */
+    unsigned char arp_hln;		/* Length of hardware address.  */
+    unsigned char arp_pln;		/* Length of protocol address.  */
+    unsigned short int arp_op;		/* ARP opcode (command).  */
+    struct eth_addr arp_sha;    /* Sender hardware address.  */
+    struct ip_addr arp_sip;     /* Sender IP address.  */
+    struct eth_addr arp_tha;    /* Target hardware address.  */
+    struct ip_addr arp_tip;     /* Target IP address.  */
+  };
+
+
+/* ARP protocol HARDWARE identifiers. */
+#define ARPHRD_NETROM	0		/* From KA9Q: NET/ROM pseudo. */
+#define ARPHRD_ETHER 	1		/* Ethernet 10/100Mbps.  */
+#define	ARPHRD_EETHER	2		/* Experimental Ethernet.  */
+#define	ARPHRD_AX25	3		/* AX.25 Level 2.  */
+#define	ARPHRD_PRONET	4		/* PROnet token ring.  */
+#define	ARPHRD_CHAOS	5		/* Chaosnet.  */
+#define	ARPHRD_IEEE802	6		/* IEEE 802.2 Ethernet/TR/TB.  */
+#define	ARPHRD_ARCNET	7		/* ARCnet.  */
+#define	ARPHRD_APPLETLK	8		/* APPLEtalk.  */
+#define	ARPHRD_DLCI	15		/* Frame Relay DLCI.  */
+#define	ARPHRD_ATM	19		/* ATM.  */
+#define	ARPHRD_METRICOM	23		/* Metricom STRIP (new IANA id).  */
+#define ARPHRD_IEEE1394	24		/* IEEE 1394 IPv4 - RFC 2734.  */
+#define ARPHRD_EUI64		27		/* EUI-64.  */
+#define ARPHRD_INFINIBAND	32		/* InfiniBand.  */
+
+typedef struct {
+    struct eth_hdr ether;
+    struct ip_hdr ipv4;
+    struct udp_hdr udp;
+    memcached_udp_header mcudp;
+    protocol_binary_request_header mcr;
+    uint8_t payload[];
+} __attribute__ ((packed)) iokvs_message;
+
+typedef struct {
+    struct eth_hdr ether;
+    struct ip_hdr ipv4;
+    struct udp_hdr udp;
+    char cmd[5];
+    uint8_t payload[];
+} __attribute__ ((packed)) udp_message;
+
+#endif
---
+++ compiler/benchmarks_ming/request.py
@@ -0,0 +1,144 @@
+from dsl2 import *
+from compiler import Compiler
+import target, queue2, net_real, library_dsl2
+
+
+class Request(Element):
+    def configure(self):
+        self.inp = Input(Size, "void*", "void*")
+        self.out = Output(Size, "void*", "void*")
+
+    def impl(self):
+        self.run_c(r'''
+(size_t size, void* pkt, void* buff) = inp();
+udp_message* m = (udp_message*) pkt;
+
+m->ether.src = src;
+m->ether.dest = dest;
+
+m->ipv4.src = src_ip;
+m->ipv4.dest = dest_ip;
+
+static __thread uint16_t sport = 0;
+m->udp.src_port = (++sport == 0 ? ++sport : sport);
+m->udp.dest_port = m->udp.src_port;
+
+m->ether.type = htons(ETHERTYPE_IPv4);
+m->ipv4._proto = 17; // udp
+        m->ipv4._len = htons(size - offsetof(iokvs_message, ipv4));
+        m->ipv4._ttl = 64;
+        m->ipv4._chksum = 0;
+        //m->ipv4._chksum = rte_ipv4_cksum(&m->ipv4);  // TODO
+
+        m->udp.len = htons(size - offsetof(iokvs_message, udp));
+        m->udp.cksum = 0;
+        //printf("sizeof(iokvs) = %d, size = %ld\n", sizeof(iokvs_message), size);
+
+output { out(size, pkt, buff); }
+        ''')
+
+class PayloadGen(Element):
+    def configure(self):
+        self.inp = Input(Size, "void*", "void*")
+        self.out = Output(Size, "void*", "void*")
+
+    def impl(self):
+        self.run_c(r'''
+(size_t size, void* pkt, void* buff) = inp();
+
+udp_message* m = (udp_message*) pkt;
+int i;
+
+strcpy(m->cmd, "HASH");
+for(i=0; i<4; i++) {
+    sprintf(m->payload + 8*i, "%d", TEXT_BASE + rand() % TEXT_BASE);
+}
+
+output { out(size, pkt, buff); }
+        ''')
+
+class Stat(State):
+    count = Field(Size)
+    lasttime = Field(Size)
+
+    def init(self):
+        self.count = 0
+        self.lasttime = 0
+
+class Reply(Element):
+    this = Persistent(Stat)
+
+    def configure(self):
+        self.inp = Input(Size, "void*", "void*")
+        self.out = Output("void*", "void*")
+
+    def states(self):
+        self.this = Stat()
+
+    def impl(self):
+        self.run_c(r'''
+(size_t size, void* pkt, void* buff) = inp();
+iokvs_message* m = (iokvs_message*) pkt;
+
+
+if(m->mcr.request.magic == PROTOCOL_BINARY_RES) {
+    //printf("pkt\n");
+uint64_t mycount = __sync_fetch_and_add64(&this->count, 1);
+if(mycount == 5000000) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    size_t thistime = now.tv_sec * 1000000 + now.tv_usec;
+    printf("%zu pkts/s  %zu Gbits/s\n", (mycount * 1000000)/(thistime - this->lasttime),
+                                        (mycount * size * 2 * 8)/((thistime - this->lasttime) * 1000));
+    this->lasttime = thistime;
+    this->count = 0;
+}
+}
+
+output { out(pkt, buff); }
+        ''')
+
+
+class gen(InternalLoop):
+    def impl(self):
+        net_alloc = net_real.NetAlloc()
+        to_net = net_real.ToNet(configure=["net_alloc"])
+
+        library_dsl2.Constant(configure=[80]) >> net_alloc
+        net_alloc.oom >> library_dsl2.Drop()
+        net_alloc.out >> Request() >> PayloadGen() >> to_net
+
+class recv(InternalLoop):
+    def impl(self):
+        from_net = net_real.FromNet()
+        free = net_real.FromNetFree()
+
+        from_net.nothing >> library_dsl2.Drop()
+
+        from_net >> Reply() >> free
+
+n = 5
+gen('gen', process='dpdk', cores=range(n))
+recv('recv', process='dpdk', cores=range(n))
+c = Compiler()
+c.include = r'''
+#include <string.h>
+#include "protocol_binary.h"
+
+struct eth_addr src = { .addr = "\x68\x05\xca\x33\x13\x40" };
+struct eth_addr dest = { .addr = "\x68\x05\xca\x33\x11\x3c" };
+struct ip_addr src_ip = { .addr = "\x0a\x03\x00\x1e" };   // n30
+struct ip_addr dest_ip = { .addr = "\x0a\x03\x00\x21" };  // n33
+
+#define TEXT_BASE 10000000 /* 10M (8 bits) */
+typedef enum _TYPE {
+    ECHO, /* echo */
+    HASH, /* hash computing */
+    FLOW, /* flow classification */
+    SEQU, /* sequencer */
+} PKT_TYPE;
+
+#define CMD HASH
+'''
+c.testing = 'while (1) pause();'
+c.generate_code_and_compile()
